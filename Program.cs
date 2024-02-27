c1d928bb7c3d1fd5997b08d847c59cc3b6054b69
Polinaaaaaaaaa1 <158997747+Polinaaaaaaaaa1@users.noreply.github.com>
2024-02-27 16:58:48 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,57 +14,58 @@ namespace _3_3
             Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("длина одномерного массива");
             int length = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите true, если заполните массив  с клавиатуры, иначе false");
+            bool id = bool.Parse(Console.ReadLine());
             one_dimensional Array = new one_dimensional(length);
+            Array.fill_select(id);
             
 
             Console.Write("введите длину многомерного массива");
             int length2 = int.Parse(Console.ReadLine());
             Console.Write("введите ширину многомерного массива");
             int heigth2 = int.Parse(Console.ReadLine());
- 
+            Console.WriteLine("Введите true, если заполните массив  с клавиатуры, иначе false");
+            bool id2 = bool.Parse(Console.ReadLine());
             multidimensional Array2 = new multidimensional(length2, heigth2);
-            
+            Array2.fill_select(id2);
 
-           
+
 
 
             Console.WriteLine("введите количествео строк зубчатого массива");
             int heigth3 = int.Parse(Console.ReadLine());
-        
+            
             int[] array_of_lengths = new int[heigth3];
             for (int i = 0; i < heigth3; i++)
             {
                 Console.WriteLine($"введите количество эл-тов в {i + 1} строке зубч массива");
                 array_of_lengths[i] = int.Parse(Console.ReadLine());
             }
+            Console.WriteLine("Введите true, если заполните массив  с клавиатуры, иначе false");
+            bool id3 = bool.Parse(Console.ReadLine());
             jagged Array3 = new jagged(heigth3, array_of_lengths);
+            Array3.fill_select(id3);
 
 
-            base_array[] arrays = { Array, Array2, Array3 };
+            IPRINTER[] arrays = { Array, Array2, Array3 };
             for (int i = 0; i < arrays.Length; i++)
             {
-                Console.WriteLine($"Введите true, если заполните массив {i+1} с клавиатуры, иначе false");
-                bool id = bool.Parse(Console.ReadLine());
-                if(id==true)
-                {
-
-                    arrays[i].user_fill();
-                }
-                else
-                {
-                        arrays[i].random_fill();
-                }
-                arrays[i].average();
                 Console.WriteLine("обычный вывод массива");
-                arrays[i].print();
+                arrays[i].Print();
                 
+            }
+
+            IBaseArray[] arrays2 = { Array, Array2, Array3 };
+            for (int i = 0; i < arrays2.Length; i++)
+            {
+                arrays2[i].average();
+
             }
             Array.delete();
             Array2.print_snake();
             Array3.replace_even();
 
-            string days = "понедельник,вторник,среда,четверг,пятница,суббота,воскресенье";
-            days str = new days(days);
+            days str = new days();
             str.Print();
         }
     }
